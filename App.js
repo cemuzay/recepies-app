4f18ca3ff2523d950331919ce238c15e328f37b7
cem <85490666+cemuzay@users.noreply.github.com>
2023-08-24 00:34:34 +0300

    commit

--- App.js
+++ App.js
@@ -1,21 +1,21 @@
-import React, { useState } from 'react';
-import { View, Text, TouchableOpacity, StyleSheet, SafeAreaView,Image, ScrollView } from 'react-native';
-import { FontAwesome } from '@expo/vector-icons';
+import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import Home from './Home';
-const Tab =createBottomTabNavigator();
+import HomeScreen from './HomeScreen';
+import { Title } from 'react-native-paper';
+
+const Tab = createBottomTabNavigator();
+
 export default function App() {
-
-
   return (
-    <SafeAreaView>
     <NavigationContainer>
       <Tab.Navigator>
-        <Tab.Screen name="Home" component={Home}/>
+        <Tab.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ title: "Anasayfa" }}
+        />
       </Tab.Navigator>
     </NavigationContainer>
-      
-    </SafeAreaView>
   );
 }
